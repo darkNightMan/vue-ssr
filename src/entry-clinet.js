26d16f625a89e773f05ddc1a3e3d504d33a5e881
王希平 <wangxiping@banggood.com>
2019-04-11 15:31:50 +0800

    修改

--- src/entry-clinet.js
+++ src/entry-clinet.js
@@ -8,7 +8,6 @@ Vue.mixin({
   beforeMount () {
     const { asyncData } = this.$options
     if (asyncData) {
-      debugger
       // 将获取数据操作分配给 promise
       // 以便在组件中，我们可以在数据准备就绪后
       // 通过运行 `this.dataPromise.then(...)` 来执行其他任务
